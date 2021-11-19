d949030d3b5c9fa7f2ba07dad0c3f159bb809ca4
Noah Luyt <nfluyt@gmail.com>
2021-11-18 19:32:21 -0500

    Servo Testing

--- main/main.ino
+++ main/main.ino
@@ -1,33 +1,30 @@
-#include <Servo.h> // include the Servo library
+#include <Wire.h> // Allows communication with servo
+#include <Adafruit_PWMServoDriver.h> // include the Servo library
 #include <Stepper.h> // include the Stepper library
 
-// -----NEED TO CHANGE VALUES-----test
+//Stepper Setup
 #define STEPS 100 // steps per revolution of stepper motor
-const int gripperPin = 1; // gripper servo pin
-const int telescopePin = 2; // telescoping servo pin
-const int armAnglePin = 3; // arm angle servo pin
 const int analogPin = 0; // analog in pin for stepper
 const int baseSpeed = 50; // RPM of base stepper
+// creating stepper objects to control steppers
+Stepper base(STEPS, 7, 8, 9, 10); // assume stepper is connected to pins 7-10
 
 const float minMaxArmLength[] = {50, 100}; // [mm] fully retracted/extended arm lengths
 const float degPermm = 2; // how much the servo needs to turn to extend telescope by 1 mm
 const float armAngleRatio = 5; // ratio of servo rotation:arm angle
 const float baseAngleRatio = 10; // ratio of stepper rotation:base angle
-const int gripperRatio = 50; // how much servo needs to rotate to open/close gripper
-
-// creating stepper objects to control steppers
-Stepper base(STEPS, 7, 8, 9, 10); // assume stepper is connected to pins 7-10
-
-// creating servo objects to control servos
-Servo gripper;
-Servo telescope;
-Servo armAngle;
+const int gripperRatio = 100; // how much servo needs to rotate to open/close gripper
 
 // initialize variables to store motor positions
-int gripperPos;
-int telescopePos;
-int armAnglePos;
+int gripperPos = 300;   //ranges from 150 to 520
+int telescopePos = 300; //ranges from 145 to 575
+int armAnglePos = 300;  //ranges from 175 to 725
 int basePos;
+
+//Servo Setup
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+int val[3] = {armAnglePos, telescopePos, gripperPos}; // Variable holds the value of each servo. Currently all set at a middle value
+// use convention {armAngle, Telescope, Gripper} for the servos
 
 // variables to set up spherical coords
 float theta = 0; // [deg] base angle
@@ -35,23 +32,36 @@ float phi = 0; // [deg] arm angle (assume starting in upright pos - z axis)
 float armLength = minMaxArmLength[0]; // assume arm is initially fully retracted
 int gripperState = 0; // 0 for closed, 1 for open
 
+String valInput; //for testing
+int i=0;
+int j=0; //loop indexing variable
+
 void setup() {
   base.setSpeed(baseSpeed);
 
-  // attach servos to respective pins
-  gripper.attach(gripperPin);
-  telescope.attach(telescopePin);
-  armAngle.attach(armAnglePin);
-
-  // read current motor positions
-  gripperPos = gripper.read();
-  telescopePos = telescope.read();
-  armAnglePos = armAngle.read();
   basePos = analogRead(analogPin);
 
   Serial.begin(9600); // initialize the serial port
+  Serial.setTimeout(10); // change default (1000ms) to have faster response time
+  Serial.println("Running example: Servo motor actuation using messaging");
+  pwm.begin();
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+
+  //Set servo to starting values
+  Serial.print(" Servo values = [ ");
+  for (i=0; i<3; i++) {
+    Serial.print(val[i]);
+    Serial.print(" ");
+  }
+  Serial.print("]");
+      
+  //send values to servo
+  for (i=0; i<3; i++) {
+    pwm.setPWM(i+1, 0, val[i]);
+    }
 }
 
+ 
 void moveBaseAngle(float angle) { // [deg] rotate base
   theta = theta + angle; // update coords
   angle = angle * baseAngleRatio; // convert to stepper angles
@@ -63,29 +73,62 @@ void moveBaseAngle(float angle) { // [deg] rotate base
 
 void moveArmAngle(float angle) { // [deg] rotate arm
   phi = phi + angle; // update coords
-  angle = angle * armAngleRatio; // convert to servo angles
+  //removed for the test, currently recieved angle is the change in PWM signal
+  //angle = angle * armAngleRatio; // convert to servo angles
 
   armAnglePos = armAnglePos + angle;
-  armAngle.write(armAnglePos);
+
+  //Move servo
+  for (i=0; i<abs(angle)+10; i++){
+    if (val[0] < armAnglePos){
+      val[0] = val[0]+1;
+    }
+    else if (val[0] > armAnglePos) {
+      val[0] = val[0]-1;
+    }
+    pwm.setPWM(1, 0, val[0]);
+    delay(100);
+  }
 }
 
 void moveTelescope(float dist) { // [mm] extend/retract arm
   armLength = armLength + dist; // update coords
-  dist = dist * degPermm; // convert to servo angles
+  //removed for the test, currently recieved dist is the change in PWM signal
+  //dist = dist * degPermm; // convert to servo angles
 
   telescopePos = telescopePos + dist;
-  telescope.write(telescopePos);
+
+  //Move Servo
+  for (i=0; i<abs(dist)+10; i++){
+    if (val[1] < telescopePos){
+      val[1] = val[1]+1;
+    }
+    else if (val[1] > telescopePos) {
+      val[1] = val[1]-1;
+    }
+    pwm.setPWM(2, 0, val[1]);
+    delay(100);
+  }
 }
 
 void moveGripper(int state) { // 0 to close, 1 to open
   if (state == 0 && state != gripperState) { // closing gripper
-    gripperState = state;
+    gripperState = state; //Update gripper state
     gripperPos = gripperPos - gripperRatio; // assuming subtract for closing (flip above if needed)
-    gripper.write(gripperPos);
   } else if (state == 1 && state != gripperState) { // opening gripper
-    gripperState = state;
+    gripperState = state; //Update gripper state
     gripperPos = gripperPos + gripperRatio;
-    gripper.write(gripperPos);
+  }
+  //Move Servo
+  for (i=0; i<abs(gripperRatio)+10; i++){
+    if (val[2] < gripperPos){
+      val[2] = val[2]+1;
+    }
+    else if (val[2] > gripperPos) {
+      val[2] = val[2]-1;
+    }
+    pwm.setPWM(3, 0, val[2]);
+    delay(100);
   }
 }
 
@@ -95,4 +138,52 @@ bool validMove(float newTheta, float newPhi, float newArmLength) { // TODO: vali
 
 void loop() {
   // TODO: put your main code here, to run repeatedly:
+  
+  //Test
+  if (Serial.available() > 0) {
+    valInput = Serial.readString();
+    Serial.print("I recived: ");
+    Serial.print(valInput);
+
+    //Uncomment any of the code below to test each servo
+    //moveArmAngle(valInput.toInt());
+    //moveTelescope(valInput.toInt());
+    moveGripper(valInput.toInt());
+    
+    //Print current servo vales
+    Serial.print(" Servo values = [ ");
+    for (i=0; i<3; i++) {
+      Serial.print(val[i]);
+      Serial.print(" ");
+    }
+    Serial.print("]");
+        
+    //send values to servo
+    for (i=0; i<3; i++) {
+      pwm.setPWM(i+1, 0, val[i]);
+      }
+  }//test ends
+
+
+  //Standy I (waiting for coordinates of pickup)
+
+  //Check Movement (recived coords, using valid move function to check if the move will work)
+
+  //Grab
+    //Rotate base
+    //lower and extend arm (Simultaneously?)
+    //Grip test tube
+    //Return to middle position
+
+  //Sandby II (waiting for coordinates of dropoff)
+
+  //Check Movement (recived coords, using valid move function to check if the move will work)
+  
+  //Place
+    //Rotate base
+    //lower and extend arm (Simultaneously?)
+    //Release test tube
+    //Return to middle position  
+
+  //Loop back to top
 }
